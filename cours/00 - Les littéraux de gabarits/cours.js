5a2ff6b33e7070a9be33bc3682c42d981a1c96d1
JavaScript de Zéro <jeremy@javascriptdezero.com>
2019-06-18 15:20:39 +0200

    Premier brouillon du cours sur les littéraux de gabarits

---
+++ cours/00 - Les littéraux de gabarits/cours.js
@@ -0,0 +1,56 @@
+/* Les littéraux de gabarits (template strings | literals) */
+
+// Des noms différents pour désigner la même chose...
+/*
+littéraux de gabarits
+gabarits de libellés
+gabarits de chaînes de caractères
+modèles de libellés
+template strings
+template literals
+*/
+
+// On les a déjà vu !
+console.log(`C'est un "littéral de gabarit" !`);
+
+// Multi-ligne
+// console.log("Première ligne
+// Deuxième ligne");
+
+// console.log(`Première ligne
+// Deuxième ligne`);
+
+// Interpolation d'expressions
+// let nombreEmails = 42;
+// let prenom = "Jérémy";
+
+// console.log(`Bienvenue ${prenom} !
+// Vous avez ${nombreEmails} emails non lus`);
+
+// Interpolation d'expressions complexes 1
+// let nombreEmails = 40;
+// let nombreSMS = 2;
+
+// console.log(`Bienvenue Jérémy !
+// Vous avez ${nombreEmails + nombreSMS} messages non lus`);
+
+// Interpolation d'expressions complexes 2
+// let nombreHeures = 3;
+
+// console.log(`Il y a ${nombreHeures * 60} minutes dans ${nombreHeures} heures`);
+
+// Utilisation de fonctions
+// function saluer(heure) {
+//   if (heure >= 6 && heure <= 18) {
+//     return "Bonjour";
+//   }
+//   return "Bonsoir";
+// }
+
+// console.log(`${saluer(20)} Jérémy !`);
+
+// Quelle utilité ?
+// Construire du code HTML formaté qui contiendra des valeurs de variables en JS.
+// Similaire à JSX
+
+// Toujours utiliser les backticks ?
