3b5a1487e3a65ecb705a0ac11c79c8dd4726fc1e
JavaScript de Zéro <jeremy@javascriptdezero.com>
2019-07-29 16:47:50 +0200

    Première version du cours sur else if et switch

---
+++ cours/04 - Structures conditionnelles : else if et switch/cours.js
@@ -0,0 +1,48 @@
+/* Structures conditionnelles : else if et switch */
+
+/* Le raccourci "else if" */
+// Rappel
+let condition = false;
+
+if (condition) {
+  console.log("La condition est vraie");
+} else {
+  console.log("La condition est fausse");
+}
+
+// Autoriser certains accès sur un forum
+// let typeUtilisateur = "modérateur";
+
+// if (typeUtilisateur === "administrateur") {
+//   console.log("Accès autorisés : administration + modération + création de contenu");
+// } else {
+//   if (typeUtilisateur === "modérateur") {
+//     console.log("Accès autorisés : modération + création de contenu");
+//   } else {
+//     if (typeUtilisateur === "membre") {
+//       console.log("Accès autorisés : création de contenu");
+//     } else {
+//       console.log("Les visiteurs peuvent uniquement lire le forum");
+//     }
+//   }
+// }
+
+/* La structure switch */
+// let typeUtilisateur = "modérateur";
+
+// switch (typeUtilisateur) {
+//   case "administration":
+//     console.log("Accès autorisés : administration + modération + création de contenu");
+//     break;
+//   case "modérateur":
+//     console.log("Accès autorisés : modération + création de contenu");
+//     break;
+//   case "membre":
+//     console.log("Accès autorisés : création de contenu");
+//     break;
+//   default:
+//     console.log("Les visiteurs peuvent uniquement lire le forum");
+//     break;
+// }
+
+// console.log("Fin du code");
