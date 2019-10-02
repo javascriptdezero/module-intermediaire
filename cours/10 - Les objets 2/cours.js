67e9c4e3c91765c6ebbdf0bddef9fd79cb40812c
JavaScript de Zéro <jeremy@javascriptdezero.com>
2019-10-02 09:38:09 +0200

    Brouillon du cours sur les objets 2

---
+++ cours/10 - Les objets 2/cours.js
@@ -0,0 +1,26 @@
+/* Les objets (partie 2) */
+
+/* Objet vide */
+let personne = {};
+console.log(personne);
+
+/* Lecture d'une propriété inexistante */
+// console.log(personne.prenom);
+// console.log(personne["age"]);
+
+/* Ajout dynamique d'une propriété */
+// personne.prenom = "Jérémy";
+// personne["age"] = 34;
+// personne.profession = "développeur";
+// console.log(personne);
+
+/* Suppression dynamique d'une propriété */
+// delete personne.prenom;
+// delete personne["age"];
+// console.log(personne);
+
+/* Différences classe VS prototype */
+
+/* Subtilité avec console.log() */
+// console.log("Avec + : " + personne);
+// console.log("Avec , : ", personne);
