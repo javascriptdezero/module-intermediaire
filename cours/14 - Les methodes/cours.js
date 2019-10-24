4d4d2f978ce815f56e21b71cda86ef95ff0b4833
JavaScript de Zéro <jeremy@javascriptdezero.com>
2019-10-24 15:23:24 +0200

    Version finale avant tournage du cours sur les méthodes

--- cours/14 - Les methodes/cours.js
+++ cours/14 - Les methodes/cours.js
@@ -1,36 +1,53 @@
 /* Les méthodes : des fonctions dans des objets */
 
-// const login = {
-//   twitter: "@JeremyMouzin",
-//   nom: "Jérémy Mouzin",
-//   afficher: function() {
-//     console.log("Le login twitter @JeremyMouzin appartient à Jérémy Mouzin.");
-//   },
-// };
-
-// login.afficher();
-
-/* Utiliser les propriétés d'un objet dans une méthode */
-
-const login = {
+/* Définition d'une méthode (VS une fonction) */
+const identifiant = {
   twitter: "@JeremyMouzin",
   nom: "Jérémy Mouzin",
   afficher: function() {
-    console.log(`Le login twitter ${this.twitter} appartient à ${this.nom}.`);
+    console.log("L'identifiant twitter @JeremyMouzin appartient à Jérémy Mouzin.");
   },
 };
 
-login.afficher();
-login.nom = "Tom Redocemia";
-login.afficher();
+// Notation pointée
+identifiant.afficher();
+// Notation avec les crochets
+// identifiant["afficher"]();
 
+/* Lire les propriétés d'un objet depuis une méthode */
+
+// const identifiant = {
+//   twitter: "@JeremyMouzin",
+//   nom: "Jérémy Mouzin",
+//   afficher: function() {
+//     console.log(`L'identifiant twitter ${twitter} appartient à ${nom}.`);
+//   },
+// };
+
+// identifiant.afficher();
+// identifiant.nom = "Tom Redocemia";
+// identifiant.afficher();
+
+/* Modifier les propriétés d'un objet depuis une méthode */
+
+// const identifiant = {
+//   twitter: "@JeremyMouzin",
+//   nom: "Jérémy Mouzin",
+//   modifierNom: function(nouveauNom) {
+//     this.nom = nouveauNom;
+//   },
+//   afficher: function() {
+//     console.log(`L'identifiant twitter ${twitter} appartient à ${nom}.`);
+//   }
+// };
+
+// identifiant.afficher();
+// identifiant.modifierNom("Tom Redocemia");
+// identifiant.afficher();
 
 /* Vous avez déjà utilisé une méthode ! */
 
 // console.log("log est une méthode de l'objet console !");
-// console.log(console);
 // console.log(typeof console);
 
-/* Documentation console */
-
-// console.error("Ceci est une erreur");
+// Doc MDN sur la console : https://developer.mozilla.org/fr/docs/Web/API/Console
