54cb9ecd26db0e45cf82c1c9b4b2f0962c0fb2f6
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-07-15 13:32:33 +0200

    Ajout d'instructions et de commentaires pour les exercices et la solution

---
+++ cours/27 - Exercice 1 - incrementation/projet.js
@@ -0,0 +1,11 @@
+/* 
+ * Rédigez votre code pour répondre aux exercices ici :
+ * 1. Bouton d'incrémentation : lorsqu'on clique
+ * sur le bouton '+' le compteur doit être incrémenté d'1 unité.
+ * 2. Bouton de décrémentation : lorsqu'on clique sur le bouton
+ * '-' le compteur doit être décrémenté d'1 unité.
+ * 3. Bouton RESET : lorsqu'on clique sur le bouton 'Reset' le
+ * compteur doit prendre la valeur zéro.
+ * 
+ * Prenez votre temps et bon courage 😁
+ */
