d8b49ef3d32066897ca160cc5e2926c1107ea0db
Jérémy Mouzin <jeremy@javascriptdezero.com>
2020-08-04 11:11:25 +0200

    Suppression de la solution de decrementation et reset du dossier incrementation

--- cours/27 - Exercice 1 - incrementation/projet-solution.js
+++ cours/27 - Exercice 1 - incrementation/projet-solution.js
@@ -1,8 +1,6 @@
 /* Une solution possible */
 
 const boutonPlus = document.querySelector('#bouton-plus');
-const boutonMoins = document.querySelector('#bouton-moins');
-const boutonReset = document.querySelector('#bouton-reset');
 const compteur = document.querySelector('#compteur');
 
 let compteurDeClics = 0;
@@ -11,18 +9,5 @@ let compteurDeClics = 0;
 boutonPlus.addEventListener('click', function() {
   // Ajoute 1 unité à compteurDeClics
   compteurDeClics++;
-  compteur.textContent = compteurDeClics;
-});
-
-/* Fonctionnalité de décrémentation */
-boutonMoins.addEventListener('click', function() {
-  // Soustrait 1 unité à compteurDeClics
-  compteurDeClics--;
-  compteur.textContent = compteurDeClics;
-});
-
-/* Bouton reset (remise à zéro) */
-boutonReset.addEventListener('click', function() {
-  compteurDeClics = 0;
   compteur.textContent = compteurDeClics;
 });
--- cours/27 - Exercice 1 - incrementation/projet.js
+++ cours/27 - Exercice 1 - incrementation/projet.js
@@ -1,11 +1,8 @@
 /* 
- * Rédigez votre code pour répondre aux exercices ici :
+ * Rédigez le code pour ajouter cette fonctionnalité :
+ * 
  * 1. Bouton d'incrémentation : lorsqu'on clique
  * sur le bouton '+' le compteur doit être incrémenté d'1 unité.
- * 2. Bouton de décrémentation : lorsqu'on clique sur le bouton
- * '-' le compteur doit être décrémenté d'1 unité.
- * 3. Bouton RESET : lorsqu'on clique sur le bouton 'Reset' le
- * compteur doit prendre la valeur zéro.
  * 
  * Prenez votre temps et bon courage 😁
  */
