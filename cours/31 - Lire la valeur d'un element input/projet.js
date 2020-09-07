90a550415764538dce0755205cf7a6bc3e04516c
Jérémy Mouzin <jeremy@javascriptdezero.com>
2020-09-07 11:31:04 +0200

    Début de brouillon de la leçon 31

---
+++ cours/31 - Lire la valeur d'un element input/projet.js
@@ -0,0 +1,5 @@
+/* Lire la valeur d'un élément input */
+
+// Attention : il n'y a pas de lien continu entre la variable et la valeur de l'input !
+// let v = input.value; => v ne changera pas quand on modifiera la valeur de l'input !
+// Il faudra relire la valeur à chaque fois
