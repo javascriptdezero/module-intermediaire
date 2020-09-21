42ed8a25e38363e6f7b54feb83d470bf27d607f7
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-09-21 11:54:51 +0200

    Cours 33 sur afficher la notification

---
+++ cours/33 - Afficher la notification/projet.js
@@ -0,0 +1,19 @@
+/* Comment afficher une notification */
+
+// Visite du CSS : classes .notification et .afficher
+
+/* Comment ajouter la classe afficher ? */
+const boutonMoins = document.querySelector('#bouton-moins');
+const notification = document.querySelector('#notification');
+
+boutonMoins.addEventListener('click', function() {
+  
+});
+
+/* Comment exécuter du code JavaScript après un certain temps ? */
+// boutonMoins.addEventListener('click', function() {
+//   notification.classList.add('afficher');
+//   // Après 3 secondes, retirer la classe afficher
+// });
+
+/* Prochain exercice */
