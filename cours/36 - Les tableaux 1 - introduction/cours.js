7a0905d97db2377b68676d9eb2fbba74779cc5e2
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-10-01 14:03:32 +0200

    Simplification tableaux 1 introduction

--- cours/36 - Les tableaux 1 - introduction/cours.js
+++ cours/36 - Les tableaux 1 - introduction/cours.js
@@ -3,33 +3,33 @@
 /* Qu'est-ce qu'un tableau et à quoi ça sert ? */
 
 /* Déclaration de tableaux (Arrays en anglais) */
-let tableauVide = [];
+let vide = [];
 
 // 4 éléments/items, zero-based index
-let tableauNombres = [12, 24, 32, 48];
-// index               0   1   2   3
+let nombres = [12, 24, 32, 48];
+// index        0   1   2   3
 
 /* Déclaration sur une ligne ou plusieurs lignes */
-let tableauPrenoms = ["marc", "marie", "paul"];
+let prenoms = ["marc", "marie", "paul"];
 
 // Déclaration identique, trailing comma (virgule finale)
-let tableauPrenomsPlusieursLignes = [
+let prenomsMultiLignes = [
   "marc",
   "marie",
   "paul" // <- Virgule de fin possible
 ];
-console.log(tableauPrenomsPlusieursLignes);
+console.log(prenomsMultiLignes);
 
-/* Des types de données différents pour les éléments */
+/* Des éléments aux types de données différents */
 let mix = [42, "bonjour", true, undefined, null, { x: 12, y: 14 }, ["a", "b"]];
 // console.log(mix);
 
 /* Rappel : un élément peut être toute expression JavaScript */
 let numero = 4539;
 
-function direSalut(prenom) {
+function saluer(prenom) {
   return `Salut ${prenom} !` ;
 };
 
-let valeurs = [42 - 12, "bon" + "jour", numero, direSalut("Jérémy")];
-// console.log(valeurs);
+let expressions = [42 - 12, "bon" + "jour", numero, saluer("Jérémy")];
+// console.log(expressions);
