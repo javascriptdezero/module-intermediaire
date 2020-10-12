81f030f1bd87c83df180f0fab4e9af9b1e78f013
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-10-12 17:34:17 +0200

    Cours sur le type Number

---
+++ cours/42 - Number/cours.js
@@ -0,0 +1,40 @@
+/* Number */
+
+/* Documentation MDN du type Number */
+
+/* Propriétés statiques (boîte à outils) */
+// Constantes numériques
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// etc.
+
+/* Méthodes statiques (boîte à outils) */
+// Construction d'une instance de type Number */
+// let nombre = new Number(5.42);
+// console.log(typeof nombre);
+// console.log(nombre);
+// console.log(Number.isFinite(1/0));
+// console.log(Number.isInteger(1/3));
+
+// Attention à bien utiliser l'opérateur new !
+// let conversion = Number("42");
+// console.log(conversion);
+// console.log(typeof conversion);
+
+/* Conversion automatique (wrapper objects) */
+// let primitive = 42.1337;
+// console.log(primitive.toFixed(2));
+// console.log(typeof primitive);
+
+// console.log(42.toFixed(2));
+
+/* Number.isNaN() VS window.isNaN() (et isFinite()) */
+// console.log(Number.isNaN("bonjour"));
+// console.log(Number.isFinite(1/0));
+
+/* Méthodes simples à connaître */
+
+/* Exercices */
+
+/* Prochaine leçon */
