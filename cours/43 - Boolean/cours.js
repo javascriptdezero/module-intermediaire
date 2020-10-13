76b8de358f0c7032c311985c3a685ad379263a9b
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-10-13 11:03:46 +0200

    Cours sur le type Boolean

---
+++ cours/43 - Boolean/cours.js
@@ -0,0 +1,15 @@
+/* Boolean */
+
+/* Documentation MDN du type Boolean */
+
+/* Utilité uniquement pour les conversions de type (sans new !) */
+// Rappel : les 7 valeurs qui sont évaluées à false :
+console.log(Boolean(0));
+console.log(Boolean(-0));
+console.log(Boolean(null));
+console.log(Boolean(false));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+
+/* Prochaine leçon */
