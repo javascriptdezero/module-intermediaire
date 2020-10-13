c5ca5d47e057ff932c5a9f9df53bc3678d40466b
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-10-13 17:13:16 +0200

    Cours sur le type Date

---
+++ cours/44 - Date/cours.js
@@ -0,0 +1,18 @@
+/* Date */
+
+/* Documentation MDN du type Date */
+
+// Epoch time :nombre de ms depuis le 1er Janvier 1970
+console.log(Date.now());
+
+// Les dates sont en anglais et dans la timezone locale
+// let maintenant = new Date();
+// console.log(maintenant);
+
+// Il faut utiliser les méthodes avec "locale"
+// console.log(maintenant.toDateString());
+// console.log(maintenant.toLocaleDateString());
+
+// Librairie externe : https://momentjs.com
+
+/* Prochaine leçon */
