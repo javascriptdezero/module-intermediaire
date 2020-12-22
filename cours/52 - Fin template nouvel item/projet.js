92d520eb3c4343d88543cbb923bffd54146831f3
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-12-22 16:09:10 +0100

    Fin cours 51

---
+++ cours/52 - Fin template nouvel item/projet.js
@@ -0,0 +1,28 @@
+/* Rédigez votre code ci-dessous */
+// Récupérer une référence sur le template
+const elTemplateItem = document.querySelector('#template-item');
+const elNouvelItem = document.querySelector('#nouvel-item');
+const elListe = document.querySelector('#liste');
+
+// Détecter la soumission du formulaire
+const elForm = document.querySelector('form');
+
+elForm.addEventListener('submit', function(e) {
+  // On empêche le rechargement de la page
+  e.preventDefault();
+  
+  // Créer un élément <li> à partir du template
+  const elLi = elTemplateItem.content.cloneNode(true);
+
+  // Récupérer la valeur de l'input nouvel item
+  const nomItem = elNouvelItem.value;
+
+  // Injecter cette valeur dans l'élément <li>
+  // Sélectionner l'élément nom <p>
+  const elNom = elLi.querySelector('.nom');
+  elNom.textContent = nomItem;
+
+  // Ajouter l'élément <li> dans la liste <ul>
+  liste.append(elLi);
+  
+});
