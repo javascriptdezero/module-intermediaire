95bf8313b5cad0e26132d1cb47900cafee10822e
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-12-22 17:58:30 +0100

    Fin cours 54

---
+++ cours/55 - Validation valeur filtree/projet.js
@@ -0,0 +1,44 @@
+/* Rédigez votre code ci-dessous */
+// Récupérer une référence sur le template
+const elTemplateItem = document.querySelector('#template-item');
+const elNouvelItem = document.querySelector('#nouvel-item');
+const elListe = document.querySelector('#liste');
+
+// Détecter la soumission du formulaire
+const elForm = document.querySelector('form');
+
+elForm.addEventListener('submit', function(e) {
+  // On empêche le rechargement de la page
+  e.preventDefault();
+  
+  // Créer un élément <li> à partir du template
+  const elLi = elTemplateItem.content.cloneNode(true);
+
+  // Récupérer la valeur de l'input nouvel item
+  let nomItem = elNouvelItem.value;
+
+  // Supprimer les espaces en trop avant et après le nom
+  nomItem = nomItem.trim();
+
+  // Supprimer les espaces d'affilés en trop
+  nomItem = nomItem.replace(/ {2,}/g, " ");
+
+  // Mettre la 1ère lettre en majuscule
+  // pommes => P + ommes
+  nomItem = nomItem[0].toUpperCase() + nomItem.slice(1);
+
+  // Injecter cette valeur dans l'élément <li>
+  // Sélectionner l'élément nom <p>
+  const elNom = elLi.querySelector('.nom');
+  elNom.textContent = nomItem;
+
+  // Ajouter l'élément <li> dans la liste <ul>
+  liste.append(elLi);
+
+  // Vider le champ nouvel item
+  elNouvelItem.value = "";
+
+  // Mettre le focus immédiatement sur le champ nouvel item
+  elNouvelItem.focus();
+  
+});
