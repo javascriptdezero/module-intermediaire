709652999cd43cba6a0c3a5d549c2e0c3fbf2cee
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-12-22 15:52:16 +0100

    Fin du cours 50

---
+++ cours/51 - Template nouvel item/projet.js
@@ -0,0 +1,12 @@
+/* Rédigez votre code ci-dessous */
+
+// Détecter la soumission du formulaire
+const elForm = document.querySelector('form');
+console.log(elForm);
+
+elForm.addEventListener('submit', function(e) {
+  // On empêche le rechargement de la page
+  e.preventDefault();
+  
+  
+});
