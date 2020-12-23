671575b2599737dc69eef8baaf8f8c5538a4be8e
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-12-23 11:16:14 +0100

    Fin cours 59

---
+++ cours/60 - Insertion intelligente 1/projet.js
@@ -0,0 +1,64 @@
+/* Rédigez votre code ci-dessous */
+// Récupérer une référence sur le template
+const elTemplateItem = document.querySelector('#template-item');
+const elNouvelItem = document.querySelector('#nouvel-item');
+const elListe = document.querySelector('#liste');
+
+// Détecter la soumission du formulaire
+const elForm = document.querySelector('form');
+
+elForm.addEventListener('submit', function(e) {
+  // On empêche le rechargement de la page
+  e.preventDefault();
+  
+  // Créer un élément <li> à partir du template
+  const elLi = elTemplateItem.content.cloneNode(true);
+
+  // Récupérer la valeur de l'input nouvel item
+  let nomItem = elNouvelItem.value;
+
+  // Supprimer les espaces en trop avant et après le nom
+  nomItem = nomItem.trim();
+
+  // Supprimer les espaces d'affilés en trop
+  nomItem = nomItem.replace(/ {2,}/g, " ");
+
+  // Mettre la 1ère lettre en majuscule
+  // pommes => P + ommes
+  nomItem = nomItem[0].toUpperCase() + nomItem.slice(1);
+
+  // Injecter cette valeur dans l'élément <li>
+  // Sélectionner l'élément nom <p>
+  const elNom = elLi.querySelector('.nom');
+  elNom.textContent = nomItem;
+
+  // Ajouter l'élément <li> dans la liste <ul>
+  liste.append(elLi);
+
+  // Vider le champ nouvel item
+  elNouvelItem.value = "";
+
+  // Mettre le focus immédiatement sur le champ nouvel item
+  elNouvelItem.focus();
+  
+});
+
+elNouvelItem.addEventListener('input', function(e) {
+  console.log('input');
+  elNouvelItem.setCustomValidity('');
+  elNouvelItem.checkValidity();
+});
+
+elNouvelItem.addEventListener('invalid', function(e) {
+  console.log('invalid');
+  const nom = elNouvelItem.value;
+
+  if (nom.length === 0) {
+    elNouvelItem.setCustomValidity("Vous devez indiquer les informations de l'item, exemple : 250 g chocolat");
+  } else if (!/[A-Za-z]{2}/.test(nom)) {
+    // Si nom ne contient pas 2 lettres côte à côte
+    elNouvelItem.setCustomValidity("Le nom de l'item doit faire 2 lettres minimum");
+  } else {
+    elNouvelItem.setCustomValidity("Les caractères spéciaux, les accents et autres lettres spécifiques ne sont pas autorisés");
+  }
+});
